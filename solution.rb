632aa67211441527eafebc82cd6b25594b8dcf79
Johan Higuita <higuitajohan@gmail.com>
2018-11-02 12:59:01 -0500

    sinatra ejercicio2

--- solution.rb
+++ solution.rb
@@ -1,11 +1,8 @@
 require 'sinatra'
 
-get '/'  do
-
-	
-
-	if params[:nombre] && params[:nombre]!=""
-		"<h1>Hola #{params[:nombre]}!</h1>"
+get '/makers/:name'  do
+	if params[:name] && params[:name]!=""
+		"<h1>Hola #{params[:name].capitalize}!</h1>"
 	else
 		"<h1>Hola desconocido!</h1>"
 	end
